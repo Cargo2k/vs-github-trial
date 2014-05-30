64a0f334ff3af7749f9f385ecd4c21dd6b1a595a
Cargo2k <cargo2k@hotmail.com>
2014-05-30 00:27:06 -0700

    edit 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 /* app title : file title : description
 */
-
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+	std::cout << "Hello git hub from my vs ide" << std::endl;
+	std::cout << "a new print" << std::endl;
 	return 0;
 }
 
