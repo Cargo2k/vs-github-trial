074ad2ec59789187f4698dae2346ad0a14920eb2
Cargo2k <cargo2k@hotmail.com>
2014-05-30 00:31:29 -0700

    added a ;

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello git hub from my vs ide" << std::endl;
 	std::cout << "a new print" << std::endl;
-	std::cout << "a 3rd edit" << std::endl
+	std::cout << "a 3rd edit" << std::endl;
 	return 0;
 }
 
